c2a762e481f4bc45037e04a2971e73d91a4b955a
Florian Klien <flowolf@klienux.org>
2017-09-12 13:32:16 +0200

    added possibility to read message from stdin

--- yessssms.sh
+++ yessssms.sh
@@ -2,18 +2,22 @@
 # Fill out your Yesss Number and password.
 # Send SMS from your prepaid SIM via the web.
 # $ ./yessssms.sh 0043681123456789 "your message in quotes"
+# or
+# $ echo "message" | ./yessssms.sh 0043681123456789 -
+# ... to read from stdin
 # that's it
 
 # NOTE: this script does NOT validate your balance.
-# last tested on: 2014-08-11
+# last tested on: 2017-09-12
 
 yesss_number="0681XXXXXXXXXX"
 yesss_pw="YOUR_SECRET_YESSS_WEBLOGIN"
+#source login_data.sh
 
 #UA="Mozilla/5.0 (Windows; U; Windows NT 6.1; de; rv:1.9.1.4) Gecko/20091017 SeaMonkey/2.0"
 UA="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12"
 KMURL="https://www.yesss.at/kontomanager.at/kundendaten.php"
-mess=`echo "$2" | cut -b -160`
+
 num=$1
 RES1=`curl -s -i -A "$UA" -d "login_rufnummer=$yesss_number&login_passwort=$yesss_pw" https://www.yesss.at/kontomanager.at/index.php`
 SESSID=`echo $RES1 | grep Set-Cookie | grep PHPSESSID | sed 's/.*\(PHPSESSID=[^;]*\);.*/\1/g'`
@@ -47,8 +51,18 @@ BAL=`echo $BAL| sed 's/Verbleibend: \([0-9]*[\.,]*[0-9]*\)/\1 Minuten\/SMS/g'`
 #echo "session ID: $SESSID"
 echo "balance: $BAL$GUT"
 
+
 if ! test -z $1; then
-  test -z "$2" && echo "$0 <number with 0043650...> \"message in quotes\"" && exit -4
+  #if [ "$2" == "-" -o -z "$2" ]; then
+  if [ "$2" == "-" ]; then
+    # read from STDIN
+    echo "read from stdin"
+    mess=$(</dev/stdin)
+    mess=`echo "$mess" | cut -b -160` 
+  else
+    mess=`echo "$2" | cut -b -160`
+  fi
+  test -z "$mess" && echo "$0 <number with 0043650...> \"message in quotes\"" && exit -4
 
   echo "sending SMS..."
   echo "number: $num"
